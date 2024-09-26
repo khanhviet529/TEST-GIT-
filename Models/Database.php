e848a1a196e9ccb12c4587d852a1dba695d66017
khánh nguyễn <khanhviet472003@gmail.com>
2024-09-26 09:12:38 +0700

    update

--- Models/Database.php
+++ Models/Database.php
@@ -2,14 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-   
-
-    <h1> khanh 123 </h1>
-    <h1> khanh 4 </h1>
+    <h1> dtabase   </h1>
 </body>
 </html>
